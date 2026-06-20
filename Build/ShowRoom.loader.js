033bb8e5f535bdceb20bb5c2fc804a831d6f4b40
Pankaj68 <ppankaj.parmar@gmail.com>
2026-06-20 20:00:37 +0530

    v2

--- Build/ShowRoom.loader.js
+++ Build/ShowRoom.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 93423408,
+    wasmFileSize: 93436024,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
